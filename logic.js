89d1d95d594616af030027296dc40cd75000d30f
PAGARE-MANGESH <140226508+PAGARE-MANGESH@users.noreply.github.com>
2023-09-13 21:22:18 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,20 @@
+function toggleNav() {
+    var navbarMenu = document.getElementById("navbarMenu");
+    navbarMenu.classList.toggle("active");
+}
+
+
+//    about me
+
+// function aboutme() {
+//     let a = addEventListener('click', function () {
+//         this.confirm('thnx for click here')
+//     })
+
+// }
+// console.log(a)  
+
+
+// function hello(){
+//      alert('hello')
+// }
